c1bfeeb29326d3b648e1acd4a5e52bde7ff0acda
Mateo Ceballos <90419@sistemas.frc.utn.edu.ar>
2023-05-29 20:29:08 -0300

    Cambios menores

--- Implementacion/Diagrama de Clases de Analisis/Pregunta.py
+++ Implementacion/Diagrama de Clases de Analisis/Pregunta.py
@@ -4,40 +4,45 @@ import RespuestaPosible
 class Pregunta:
     def __init__(self, preg='', resp=None):
         self.pregunta = preg
-        self.m_respuestaPosible = resp
+        self.respuestas = resp
 
     def getDescripcion(self):
         return self.pregunta
 
     def listarRespuestasPosibles(self):
-        pass
+        return self.respuestas[0].getDescripcionRta()
 
-preguntasRandom = {
-    1: ['¿Le gustó la atención?', []],
-    2: ['¿Del 1 al 10 en cuánto nos calificaría?', []],
-    3: ['¿Nos recomendaría a otras personas?', []]
-}
+class adhoc:
+    def generarPreguntasAleatorias(cantidadPreguntas):
+        preguntasRandom = [
+            ['¿Le gustó la atención?', []],
+            ['¿Del 1 al 10 en cuánto nos calificaría?', []],
+            ['¿Nos recomendaría a otras personas?', []]
+        ]
 
-def generarPreguntasAleatorias(cantidadPreguntas):
-    adhoc = RespuestaPosible.adhoc()
-    preguntas = cantidadPreguntas * [None]
-    for i in range(cantidadPreguntas):
-        preguntas[i] = Pregunta()
-        preguntas[i].pregunta = preguntasRandom[i+1][0]
-        if preguntas[i].pregunta == preguntasRandom[2][0]:
-            preguntas[i].m_respuestaPosible = adhoc.generarRespuestas(1, 1)  # Cambié 'vector' por 2
-        else: 
-            preguntas[i].m_respuestaPosible = adhoc.generarRespuestas(1, 2)
-    return preguntas  
-    print('Preguntas generadas con éxito')
+        rtaPosibleadhoc = RespuestaPosible.adhoc()
+
+        preguntas = cantidadPreguntas * [None]
+
+        for i in range(cantidadPreguntas):
+            preguntaRandom = preguntasRandom[i][0]
+            
+            if preguntas[i].pregunta == preguntasRandom[1][0]:
+                respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 0)
+            else: 
+                respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 1)
+
+            preguntas[i] = Pregunta(preguntaRandom, respuestaPosible)
+        
+        return preguntas
 
 
 def test():
     n = int(input('Ingrese la cantidad de preguntas a generar (3 o menos): '))
-    while 0 < n > 3:
-        n = int(input('La cantidad de preguntas a generar excede la cantidad de preguntas disponibles:'))
-    #preguntas = n * [None]
-    preguntas = generarPreguntasAleatorias(n) #, preguntas)
+    while n < 0 or n > 3:
+        n = int(input('Ingrese la cantidad de preguntas a generar (3 o menos): '))
+    
+    preguntas = adhoc().generarPreguntasAleatorias(n)
     for i in range(n):
         print('Pregunta:', i + 1)
         print('Descripción:', preguntas[i].pregunta)
@@ -46,11 +51,5 @@ def test():
            print(respuesta.getDescripcionRta())
         #print(preguntas.m_respuestaPosible.getDescripcionRta(preguntas[i].m_respuestaPosible[0]))
 
-def main():
-    pass
-
 if __name__ == '__main__':
     test()
-
-# q: ¿Por que no puedo utilizar getDescripcionRta()?
-# a: Porque es un método de la clase RespuestaPosible, no de la clase Pregunta. Para poder utilizarlo, deberías crear un objeto de la clase RespuestaPosible y llamar al método sobre ese objeto.
--- Implementacion/Diagrama de Clases de Analisis/RespuestaPosible.py
+++ Implementacion/Diagrama de Clases de Analisis/RespuestaPosible.py
@@ -1,6 +1,5 @@
 import random
 
-
 class RespuestaPosible:
     def __init__(self, des='', val=''):
         self.descripcion = des
@@ -8,42 +7,39 @@ class RespuestaPosible:
 
     def getDescripcionRta(self):
         return self.descripcion
+    
+    def __str__(self):
+        r = ''
+        r += '{:<50}'.format('Descripción de la respuesta: ' + self.descripcion)
+        r += '{:<30}'.format('Valor de la respuesta: ' + str(self.valor))
+        return r
 
-# ~La clase adhoc es una clase auxiliar que se utiliza para generar respuestas posibles aleatorias
-# ~para las preguntas de la encuesta
+# La clase adhoc es una clase auxiliar que se utiliza para generar respuestas posibles aleatorias
+# para las preguntas de la encuesta
 class adhoc:
     def generarRespuestas(self, n, tipoPregunta):
         descripcionesGenerales = ['1 al 10', 'Si/No']
         valoresGenerales = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ['Si', 'No']]
         respuestas = []
-        for _ in range(n):
-            if tipoPregunta == 1:
-                descripcion = descripcionesGenerales[0]
-                valor = random.choice(valoresGenerales[0])
-            elif tipoPregunta == 2:
-                descripcion = descripcionesGenerales[1]
-                valor = random.choice(valoresGenerales[1])
+        for i in range(n):
+            descripcion = descripcionesGenerales[tipoPregunta]
+            valor = random.choice(valoresGenerales[tipoPregunta])
+
             respuestas.append(RespuestaPosible(descripcion, valor))
+
         return respuestas
 
-    def string(self, col):
-        print('Descripción de la respuesta: ', col.descripcion)
-        print('Valor de la respuesta:', str(col.valor))
-        print('')
-    
     def mostrar(self, vector):
         print('----Respuestas posibles----')
         for i in range(len(vector)):
-            print('Respuesta ', i + 1)
-            self.string(vector[i])
-
-    def __str__(self):
-        return 'RespuestaPosible: ' + self.descripcion + ' - ' + self.valor
+            print('Respuesta', i + 1)
+            print(vector[i])
 
 def main():
     n = 3
-    adhoc.generarRespuestas(n, tipoPregunta)
-    adhoc.mostrar(RespuestasDefinidas)
+    adhocRespuestaPosible = adhoc()
+    respuestasGeneradas = adhocRespuestaPosible.generarRespuestas(n, 0)
+    adhocRespuestaPosible.mostrar(respuestasGeneradas)
 
 if __name__ == '__main__':
     main()

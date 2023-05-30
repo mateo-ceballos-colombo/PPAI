cef93796eb20a830d10da248f568b4f99d15d580
Tobías Martín Bordino Coniglio <93611@sistemas.frc.utn.edu.ar>
2023-05-30 13:37:09 -0300

    Modifico Encuesta y Pregunta

--- Implementacion/Diagrama de Clases de Analisis/Pregunta.py
+++ Implementacion/Diagrama de Clases de Analisis/Pregunta.py
@@ -14,10 +14,18 @@ class Pregunta:
 
 class adhoc:
     def generarPreguntasAleatorias(cantidadPreguntas):
-        preguntasRandom = [
-            ['¿Le gustó la atención?', []],
-            ['¿Del 1 al 10 en cuánto nos calificaría?', []],
-            ['¿Nos recomendaría a otras personas?', []]
+        preguntasRandomBool = [
+            ['¿Le gustó la atención?'],
+            ['¿Nos recomendaría a otras personas?'],
+            ['¿Recibiste una respuesta o solución a tu consulta o problema?'],
+            ['¿Experimentaste alguna dificultad técnica o de comunicación durante la interacción?'],
+            ['¿El personal de atención al cliente demostró conocimiento y competencia en su respuesta?']
+        ]
+        preguntasRandomNros = [
+            ['Del 1 al 10, ¿en cuánto nos calificaría?'],
+            ['Del 1 al 10, ¿qué tan fácil fue contactar a nuestro servicio de atención al cliente?'],
+            ['Del 1 al 10, ¿qué tan bien resolvimos tu consulta o problema?'],
+            ['Del 1 al 10, ¿qué tan rápido fue el tiempo de respuesta de nuestro equipo de atención al cliente?']
         ]
 
         rtaPosibleadhoc = RespuestaPosible.adhoc()
@@ -25,12 +33,13 @@ class adhoc:
         preguntas = cantidadPreguntas * [None]
 
         for i in range(cantidadPreguntas):
-            preguntaRandom = preguntasRandom[i][0]
-            
-            if preguntas[i].pregunta == preguntasRandom[1][0]:
-                respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 0)
-            else: 
-                respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 1)
+            preguntaRandom = random.sample(preguntasRandomBool + preguntasRandomNros, 1)
+
+            for j in range(preguntasRandomBool):
+                if preguntaRandom == j:
+                    respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 1)
+                else: 
+                    respuestaPosible = rtaPosibleadhoc.generarRespuestas(1, 0)
 
             preguntas[i] = Pregunta(preguntaRandom, respuestaPosible)
         
@@ -42,12 +51,12 @@ def test():
     while n < 0 or n > 3:
         n = int(input('Ingrese la cantidad de preguntas a generar (3 o menos): '))
     
-    preguntas = adhoc().generarPreguntasAleatorias(n)
+    preguntas = adhoc.generarPreguntasAleatorias(n)
     for i in range(n):
         print('Pregunta:', i + 1)
         print('Descripción:', preguntas[i].pregunta)
         print('Respuestas posibles:')
-        for respuesta in preguntas[i].m_respuestaPosible:
+        for respuesta in preguntas[i].respuestaPosible:
            print(respuesta.getDescripcionRta())
         #print(preguntas.m_respuestaPosible.getDescripcionRta(preguntas[i].m_respuestaPosible[0]))
 
